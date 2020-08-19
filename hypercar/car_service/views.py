b8939cb6d3c70eef869acd00ab08e83801bd28fd
Gaurav Patil <gauravptl023@gmail.com>
2020-08-20 01:21:15 +0530

    initial stage

---
+++ hypercar/car_service/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic.base import View
+from django.http import HttpResponse
+
+# Create your views here.
+
+class MainPage(View):
+
+
+    def get(self, requests , *args , **kwargs):
+
+        return HttpResponse ("<h2>Welcome to the Hypercar Service!</h2>")
+
+        
